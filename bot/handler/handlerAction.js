22e0233f4ba2311079da0b53d508ba83bbd12af0
Krisxh542 <krishera61@gmail.com>
2024-09-20 17:13:58 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -44,6 +44,82 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "😾"){
+  if(event.userID == "61553011640137","" ){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                if (err) return console.log(err);
+              });
+
+}else{
+    message.send("")
+  }
+  }
+        if(event.reaction == "🤍"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100066628636816",""){
+    message.unsend(event.messageID)
+}else{
+    message.send("")
+  }}
+        }
+        break;
+      case "typ":
+        typ();
+        break;
+      case "presence":
+        presence();
+        break;
+      case "read_receipt":
+        read_receipt();
+        break;
+      default:
+        break;
+    }
+  };
+}; createFuncMessage = global.utils.message;
+const handlerCheckDB = require("./handlerCheckData.js");
+
+module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
+  const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+
+  return async function (event) {
+    if (
+      global.GoatBot.config.antiInbox == true &&
+      (event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
+      (event.senderID || event.userID || event.isGroup == false)
+    )
+      return;
+
+    const message = createFuncMessage(api, event);
+
+    await handlerCheckDB(usersData, threadsData, event);
+    const handlerChat = await handlerEvents(event, message);
+    if (!handlerChat)
+      return;
+
+    const {
+      onAnyEvent, onFirstChat, onStart, onChat,
+      onReply, onEvent, handlerEvent, onReaction,
+      typ, presence, read_receipt
+    } = handlerChat;
+
+
+    onAnyEvent();
+    switch (event.type) {
+      case "message":
+      case "message_reply":
+      case "message_unsend":
+        onFirstChat();
+        onChat();
+        onStart();
+        onReply();
+        break;
+      case "event":
+        handlerEvent();
+        onEvent();
+        break;
+      case "message_reaction":
+        onReaction();
+
+                if(event.reaction == "😾"){
   if(event.userID == "61553011640137"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
@@ -54,7 +130,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "🤍"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100086747072197"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61553011640137"){
     message.unsend(event.messageID)
 }else{
     message.send("⛔️ Access Denied 🥸")
